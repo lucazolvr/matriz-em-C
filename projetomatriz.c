f02445571ed1fa7c06d674a02dedeb83815c618f
Lucas Oliveira <93846501+lucazolvr@users.noreply.github.com>
2024-05-17 15:27:28 -0300

    Update projetomatriz.c

--- projetomatriz.c
+++ projetomatriz.c
@@ -6,20 +6,20 @@ int matrizA [3][3], matrizB [3][3], matrizC [3][3];
 
 int main (){
 
-    printf("Informe os valores da Matriz A\n");
+    printf("Informe os valores da Matriz A\n"); //Entrada de dados da Matriz B
     for (i = 0; i < 3; i++){
         for ( j = 0; j < 3; j++){
             scanf("%d", &matrizA[i][j]);      
         }
     }
-    printf("Informe os valores da Matriz B\n");
+    printf("Informe os valores da Matriz B\n"); //Entrada de dados da Matriz B
     for (i = 0; i < 3; i++){
         for ( j = 0; j < 3; j++){
             scanf("%d", &matrizB[i][j]);      
         }
     }
 
- for (i = 0; i < 3; i++){
+ for (i = 0; i < 3; i++){ //Impressão da Matriz C
         for ( j = 0; j < 3; j++)
             printf("|%4d", matrizA [i][j] * matrizB [i][j]);
             printf("|\n");
